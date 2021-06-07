97fc410d128fd99fc2dd45becc9c0bba9490fca7
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-07 17:49:50 +0300

    Return relevant box

--- lib/database/HiveUtils.dart
+++ lib/database/HiveUtils.dart
@@ -4,16 +4,22 @@ import 'package:todo_list/datamodel/TaskDataModel.dart';
 class HiveUtils {
   static final _tasksBoxName = 'tasks';
 
-  static Box<Task>? _box;
+  static Box<Task>? _boxOfIncompleted;
+  static Box<Task>? _boxOfCompleted;
 
-  static Future<void> initBox() async =>
-      _box = await Hive.openBox(_tasksBoxName);
+  static Future<void> initBox() async {
+    _boxOfIncompleted = await Hive.openBox(_tasksBoxName + '_incompleted');
+    _boxOfCompleted = await Hive.openBox(_tasksBoxName + '_completed');
+  }
 
   static Box<Task> getBox(bool isCompleted) {
-    if (_box != null) {
-      return _box!;
-    } else {
+    if (_boxOfCompleted == null || _boxOfIncompleted == null) {
       throw Exception('Need to initialize box firstly');
+    } else {
+      if (isCompleted)
+        return _boxOfCompleted!;
+      else
+        return _boxOfIncompleted!;
     }
   }
 }
