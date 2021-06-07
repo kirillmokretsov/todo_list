51c169c8791f185eddfb051b7834be9df1bcf81a
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-07 14:45:48 +0300

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         accentColor: Colors.amberAccent,
       ),
-      home: ListPage(title: 'Flutter Demo Home Page'),
+      home: ListPage(title: 'Todos'),
     );
   }
 }
