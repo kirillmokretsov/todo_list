bc6e7b62078f0ba7fef2aac790ec5afb2c57b43f
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-07 16:48:01 +0300

    Update HiveUtils

--- lib/database/HiveUtils.dart
+++ lib/database/HiveUtils.dart
@@ -4,18 +4,16 @@ import 'package:todo_list/datamodel/TaskDataModel.dart';
 class HiveUtils {
   static final _tasksBoxName = 'tasks';
 
-  static bool _isBoxInit = false;
+  static Box<Task>? _box;
 
-  static Future<void> initBox() async {
-    _isBoxInit = true;
-    await Hive.openBox(_tasksBoxName);
-  }
+  static Future<void> initBox() async =>
+      _box = await Hive.openBox(_tasksBoxName);
 
   static Box<Task> getBox() {
-    if (!_isBoxInit) {
-      throw Exception('Need to initialize box firstly');
+    if (_box != null) {
+      return _box!;
     } else {
-      return Hive.box(_tasksBoxName);
+      throw Exception('Need to initialize box firstly');
     }
   }
 }
