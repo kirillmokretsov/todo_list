68ad19f9607d0e7d1865c6a6586cbda5b16473e9
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-07 15:42:25 +0300

    Register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_list/datamodel/TaskDataModel.dart';
 import 'package:todo_list/page/ListPage.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
   runApp(MyApp());
 }
 
