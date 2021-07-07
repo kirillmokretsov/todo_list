6e40d7f1d57a82fe713c5185dc81948381036e9f
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-07-07 11:14:28 +0300

    Separate ListPage and TasksList

--- lib/page/ListPage.dart
+++ lib/page/ListPage.dart
@@ -3,6 +3,7 @@ import 'package:hive/hive.dart';
 import 'package:todo_list/database/HiveUtils.dart';
 import 'package:todo_list/datamodel/TaskDataModel.dart';
 import 'package:todo_list/dialog/EditTaskDialog.dart';
+import 'package:todo_list/widget/TasksList.dart';
 import 'package:uuid/uuid.dart';
 
 class ListPage extends StatefulWidget {
@@ -25,61 +26,13 @@ class _ListPageState extends State<ListPage> {
     box = HiveUtils.getBox(widget.isCompleted);
   }
 
-  Widget _buildTile(BuildContext context, int index) {
-    Task? task = box.getAt(index);
-    if (task != null)
-      return Dismissible(
-        key: UniqueKey(),
-        child: ListTile(
-          leading: Checkbox(
-            value: task.isCompleted,
-            onChanged: (bool? value) {
-              HiveUtils.moveToAnotherBox(task!, indexOfDeleted: index);
-              setState(() {});
-            },
-          ),
-          title: Text(task.title),
-        ),
-        background: Container(
-          color: Colors.green,
-          child: Icon(Icons.edit),
-        ),
-        secondaryBackground: Container(
-          color: Colors.red,
-          child: Icon(Icons.delete),
-        ),
-        onDismissed: (direction) async {
-          if (direction == DismissDirection.endToStart) {
-            box.deleteAt(index);
-          } else if (direction == DismissDirection.startToEnd) {
-            final result = await showDialog(
-              context: context,
-              builder: (context) => EditTaskDialog(task!),
-            );
-            if (result is Task) {
-              box.putAt(index, result);
-              task = result;
-              setState(() {});
-            }
-          }
-        },
-      );
-    else
-      throw Exception('task at index $index is null');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.separated(
-        itemBuilder: _buildTile,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemCount: box.length,
-        padding: EdgeInsets.all(16.0),
-      ),
+      body: TasksList(widget.isCompleted),
       floatingActionButton: FloatingActionButton(
         onPressed: _addTask,
         child: Icon(Icons.add),
---
+++ lib/widget/TasksList.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:todo_list/database/HiveUtils.dart';
+import 'package:todo_list/datamodel/TaskDataModel.dart';
+import 'package:todo_list/dialog/EditTaskDialog.dart';
+
+class TasksList extends StatefulWidget {
+  final bool isCompleted;
+
+  const TasksList(this.isCompleted, {Key? key}) : super(key: key);
+
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  late Box<Task> box;
+
+  @override
+  void initState() {
+    super.initState();
+    box = HiveUtils.getBox(widget.isCompleted);
+  }
+
+  Widget _buildTile(BuildContext context, int index) {
+    Task? task = box.getAt(index);
+    if (task != null)
+      return Dismissible(
+        key: UniqueKey(),
+        child: ListTile(
+          leading: Checkbox(
+            value: task.isCompleted,
+            onChanged: (bool? value) {
+              HiveUtils.moveToAnotherBox(task!, indexOfDeleted: index);
+              setState(() {});
+            },
+          ),
+          title: Text(task.title),
+        ),
+        background: Container(
+          color: Colors.green,
+          child: Icon(Icons.edit),
+        ),
+        secondaryBackground: Container(
+          color: Colors.red,
+          child: Icon(Icons.delete),
+        ),
+        onDismissed: (direction) async {
+          if (direction == DismissDirection.endToStart) {
+            box.deleteAt(index);
+          } else if (direction == DismissDirection.startToEnd) {
+            final result = await showDialog(
+              context: context,
+              builder: (context) => EditTaskDialog(task!),
+            );
+            if (result is Task) {
+              box.putAt(index, result);
+              task = result;
+              setState(() {});
+            }
+          }
+        },
+      );
+    else
+      throw Exception('task at index $index is null');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: _buildTile,
+      separatorBuilder: (BuildContext context, int index) => Divider(),
+      itemCount: box.length,
+      padding: EdgeInsets.all(16.0),
+    );
+  }
+
+}
